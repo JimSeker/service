a3772c1f6a09a3c05d1d90b06eaabf789ec4c220
JimSeker <seker@uwyo.edu>
2014-09-04 11:51:26 -0600

    updates, added comments, and 4.4W

--- serviceDemo/gen/edu/cs4730/servicedemo/R.java
+++ serviceDemo/gen/edu/cs4730/servicedemo/R.java
@@ -24,16 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f080006;
-        public static final int btn_file=0x7f080005;
-        public static final int btn_istarth=0x7f080001;
-        public static final int btn_istartn=0x7f080002;
-        public static final int btn_sstarth=0x7f080003;
-        public static final int btn_sstartn=0x7f080004;
+        public static final int LinearLayout1=0x7f080001;
+        public static final int action_settings=0x7f080007;
+        public static final int btn_file=0x7f080006;
+        public static final int btn_istarth=0x7f080002;
+        public static final int btn_istartn=0x7f080003;
+        public static final int btn_sstarth=0x7f080004;
+        public static final int btn_sstartn=0x7f080005;
+        public static final int container=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int fragment_main=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -41,6 +43,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int hello_blank_fragment=0x7f050003;
         public static final int hello_world=0x7f050002;
     }
     public static final class style {
