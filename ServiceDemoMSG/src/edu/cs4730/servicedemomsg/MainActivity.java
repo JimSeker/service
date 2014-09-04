bbe2a060502231f7484d072c0fd384e4e3dd64f2
JimSeker <seker@uwyo.edu>
2014-09-04 13:55:50 -0600

    added a service using messenger.

---
+++ ServiceDemoMSG/src/edu/cs4730/servicedemomsg/MainActivity.java
@@ -0,0 +1,20 @@
+package edu.cs4730.servicedemomsg;
+
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+public class MainActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+			.add(R.id.container, new MainFragment()).commit();
+		}
+	}
+
+}
