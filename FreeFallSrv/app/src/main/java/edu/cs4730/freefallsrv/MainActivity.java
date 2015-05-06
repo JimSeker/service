ae0e615555ee3c444674f4d994c1acf173c5ee4b
JimSeker <seker@uwyo.edu>
2015-05-06 10:02:43 -0600

    Added new example of a service.

---
+++ FreeFallSrv/app/src/main/java/edu/cs4730/freefallsrv/MainActivity.java
@@ -0,0 +1,52 @@
+package edu.cs4730.freefallsrv;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/*
+ *  This is a simple demo of starting a service based on the freefall code from the sensor repo
+ *  This code only starts a service.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    Boolean started = true;
+    Button myButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startService(new Intent(this, MySensorService.class));
+        myButton = (Button)findViewById(R.id.button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (started) {
+                    stopService(new Intent(getApplicationContext(), MySensorService.class));
+                    myButton.setText("Start Service");
+                    started = false;
+                } else {
+                    startService(new Intent(getApplicationContext(), MySensorService.class));
+                    myButton.setText("Stop Service");
+                    started = true;
+                }
+            }
+        });
+
+
+    }
+    @Override
+    public void onStop() {
+        //if we don't stopService it will continue even after the activity has.
+        //in this case, the phone will continue to scream even after the activity is gone.
+        //uncomment line below, if you want to be sure, otherwise, for this example, I want to
+        //be able to leave the serivce running.
+
+        //stopService(new Intent(this,MySensorService.class));
+        super.onStop();
+    }
+}
---
+++ FreeFallSrv/app/src/main/java/edu/cs4730/freefallsrv/MySensorService.java
@@ -0,0 +1,97 @@
+package edu.cs4730.freefallsrv;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+
+/*
+ *  A "simple" service to capture sensor events and determine if the device is in free fall.
+ *  It should play a sound, but at the moment, it is just a toast.
+ *
+ *  This will likely be a pretty good drain on a battery as well.
+ */
+
+
+public class MySensorService extends Service implements SensorEventListener {
+    Context context;
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    MediaPlayer mediaPlayer;
+
+    public MySensorService() {
+    }
+
+    @Override
+    public void onCreate() {
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    //ensure that we disconnect from the sensor once it's done.
+    @Override
+    public void onDestroy() {
+        mSensorManager.unregisterListener(this, mSensor);
+        KillMediaPlayer();
+        logthis("the service in this place is dead man!");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        //SQRT(x*x + y*y + z*z).
+        double vector=Math.sqrt(event.values[0]*event.values[0]+event.values[1]*event.values[1]+event.values[2]*event.values[2]);
+        //9.8 m/s is basically not moving
+        //3.0 m/s or less is basically falling.
+        //20 m/s is landing ish, based on what I read.
+
+        //logthis("Vector is " + vector);
+
+        if (vector <=3.0) { // 3 m/s should be falling, I think...
+            //Toast.makeText(getBaseContext(),"I'm falling " + vector, Toast.LENGTH_SHORT).show();
+            playsnd();
+        }
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+       //don't care about this.
+    }
+
+    public void playsnd() {
+        if (mediaPlayer == null) { //first time
+            mediaPlayer =  MediaPlayer.create(getBaseContext(),R.raw.hmscream);
+        } else if (mediaPlayer.isPlaying()) { //duh don't start it again.
+            //Toast.makeText(getBaseContext(), "I'm playing already", Toast.LENGTH_SHORT).show();
+            return;
+        } else { //play it at least one, reset and play again.
+            mediaPlayer.seekTo(0);
+        }
+        mediaPlayer.start();
+    }
+
+
+    void KillMediaPlayer() {
+        if (mediaPlayer != null)
+            mediaPlayer.release();
+    }
+
+    public void logthis(String text) {
+        Log.v("freefall", text);
+    }
+}
