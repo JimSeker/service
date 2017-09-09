157b9b3a8a9b449d57b7e30aef4433c90e6f3a36
JimSeker <seker@uwyo.edu>
2017-09-09 14:24:42 -0600

    added a ForegroundService Example.

---
+++ ForegroundServiceDemo/app/src/main/java/edu/cs4730/foregroundservicedemo/MainActivity.java
@@ -0,0 +1,67 @@
+package edu.cs4730.foregroundservicedemo;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+/*
+ * Main activity doesn't really do much, but start the service and then finish.
+ * In oreo to run a background service when the app is not running it must
+ * startForegroundService(Intent)  in the activity
+ * in service, make a notification low or higher. persistent.
+ * and startForground (int id, Notification notification )
+*/
+
+public class MainActivity extends AppCompatActivity {
+    public static String id1 = "test_channel_01";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        createchannel();  //needed for the persistent notification created in service.
+
+        //IntentService start with 5 random number toasts
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent number5 = new Intent(getBaseContext(), MyForeGroundService.class);
+                number5.putExtra("times", 5);
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    startForegroundService(number5);
+                } else {
+                    //lower then Oreo, just start the service.
+                    startService(number5);
+                }
+                finish();
+            }
+        });
+    }
+
+    /*
+      * for API 26+ create notification channels
+      */
+    private void createchannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            NotificationChannel mChannel = new NotificationChannel(id1,
+                    getString(R.string.channel_name),  //name of the channel
+                    NotificationManager.IMPORTANCE_LOW);   //importance level
+            //important level: default is is high on the phone.  high is urgent on the phone.  low is medium, so none is low?
+            // Configure the notification channel.
+            mChannel.setDescription(getString(R.string.channel_description));
+            mChannel.enableLights(true);
+            // Sets the notification light color for notifications posted to this channel, if the device supports this feature.
+            mChannel.setShowBadge(true);
+            nm.createNotificationChannel(mChannel);
+        }
+    }
+
+}
---
+++ ForegroundServiceDemo/app/src/main/java/edu/cs4730/foregroundservicedemo/MyForeGroundService.java
@@ -0,0 +1,131 @@
+package edu.cs4730.foregroundservicedemo;
+
+import android.app.Notification;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Process;
+import android.os.Message;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Random;
+
+/*
+ * this is an example of a service that prompts itself to a foreground service with a persistent
+ * notification.  Which is now required by Oreo otherwise, a background service without an app will be killed.
+ *
+ */
+
+public class MyForeGroundService extends Service {
+
+
+    private Looper mServiceLooper;
+    private ServiceHandler mServiceHandler;
+    private final static String TAG = "MyForegroundService";
+
+    Random r;
+
+    public MyForeGroundService() {
+    }
+
+    // Handler that receives messages from the thread
+    private final class ServiceHandler extends Handler {
+        public ServiceHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            //promote to foreground and create persistent notification.
+            //in Oreo we only have a few seconds to do this or the service is killed.
+            Notification notification = getNotification("MyService is running");
+            startForeground(msg.arg1, notification);  //not sure what the ID needs to be.
+            Log.d(TAG, "should be foreground now.");
+            // Normally we would do some work here, like download a file.
+            // For our example, we just sleep for 5 seconds then display toasts.
+            //setup how many messages
+            int times = 1, i;
+
+            Bundle extras = msg.getData();
+            if (extras != null) {
+                times = extras.getInt("times", 1);  //default is one
+            }
+            //loop that many times, sleeping for 5 seconds.
+            for (i = 0; i < times; i++) {
+                synchronized (this) {
+                    try {
+                        wait(5000);
+                    } catch (InterruptedException e) {
+                    }
+                }
+                String info = i + " random " + r.nextInt(100);
+                Log.d("intentServer", info);
+                //make a toast
+                Toast.makeText(MyForeGroundService.this, info, Toast.LENGTH_SHORT).show();
+            }
+            // Stop the service using the startId, so that we don't stop
+            // the service in the middle of handling another job
+            stopSelf(msg.arg1);  //notification will go away as well.
+        }
+    }
+
+
+    @Override
+    public void onCreate() {
+        r = new Random();
+        // Start up the thread running the service.  Note that we create a
+        // separate thread because the service normally runs in the process's
+        // main thread, which we don't want to block.  We also make it
+        // background priority so CPU-intensive work will not disrupt our UI.
+        HandlerThread thread = new HandlerThread("ServiceStartArguments", Process.THREAD_PRIORITY_BACKGROUND);
+        thread.start();
+
+        // Get the HandlerThread's Looper and use it for our Handler
+        mServiceLooper = thread.getLooper();
+        mServiceHandler = new ServiceHandler(mServiceLooper);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+
+        // For each start request, send a message to start a job and deliver the
+        // start ID so we know which request we're stopping when we finish the job
+        Message msg = mServiceHandler.obtainMessage();
+        msg.arg1 = startId;//needed for stop.
+        msg.setData(intent.getExtras());
+        mServiceHandler.sendMessage(msg);
+
+        // If we get killed, after returning from here, restart
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // We don't provide binding, so return null
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
+    }
+
+    // build a persistent notification and return it.
+    public Notification getNotification(String message) {
+
+        return new NotificationCompat.Builder(getApplicationContext(), MainActivity.id1)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setOngoing(true)  //persistent notification!
+                .setChannelId(MainActivity.id1)
+                .setContentTitle("Service")   //Title message top row.
+                .setContentText(message)  //message when looking at the notification, second row
+                .build();  //finally build and return a Notification.
+    }
+}
