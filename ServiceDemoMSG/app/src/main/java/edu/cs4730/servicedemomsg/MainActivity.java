0d02408038def8993bd7eb6c1f58305f6261021b
Jim Ward <seker@uwyo.edu>
2022-04-15 10:33:34 -0600

    clean up, fixed depreciated calls

--- ServiceDemoMSG/app/src/main/java/edu/cs4730/servicedemomsg/MainActivity.java
+++ ServiceDemoMSG/app/src/main/java/edu/cs4730/servicedemomsg/MainActivity.java
@@ -1,20 +1,101 @@
 package edu.cs4730.servicedemomsg;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-//Nothing to see here, go to the MainFragment.
-
+/**
+ * This is a very simple example of a service and Ibinder (IPC)
+ * when the user clicks the button, it triggers the service to say hi via a toast.
+ * <p>
+ * http://developer.android.com/guide/components/bound-services.html
+ * http://developer.android.com/reference/android/app/Service.html
+ */
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-			.add(R.id.container, new MainFragment()).commit();
-		}
-	}
+    Messenger mService;
+    boolean mBound = false;
+    final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //setup the button, so it will call into the service via cause the service to react and say hi.
+        findViewById(R.id.btn_gn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mBound) {
+                    // Create and send a message to the service, using a supported 'what' value
+                    Message msg = Message.obtain(null, MsgService.MSG_SAY_HELLO, 0, 0);
+                    try {
+                        mService.send(msg);
+                        Log.v(TAG, "Message sent.");
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Log.v(TAG, "Bound is false");
+                }
+            }
+        });
+    }
+
+    /**
+     * Class for interacting with the main interface of the service.
+     */
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // This is called when the connection with the service has been
+            // established, giving us the object we can use to
+            // interact with the service.  We are communicating with the
+            // service using a Messenger, so here we get a client-side
+            // representation of that from the raw IBinder object.
+
+            mService = new Messenger(service);
+            mBound = true;
+            Log.v(TAG, "connected is true");
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            mService = null;
+            mBound = false;
+            Log.v(TAG, "Disconnected!");
+        }
+
+    };
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Bind to LocalService
+        Intent intent = new Intent(this, MsgService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // Unbind from the service
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
 
 }
--- ServiceDemoMSG/app/src/main/java/edu/cs4730/servicedemomsg/MsgService.java
+++ ServiceDemoMSG/app/src/main/java/edu/cs4730/servicedemomsg/MsgService.java
@@ -4,10 +4,13 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 import android.os.Message;
 import android.os.Messenger;
 import android.util.Log;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 /**
  * A simple service that receives message via a handler from the binder.
@@ -24,26 +27,22 @@ public class MsgService extends Service {
     static final int MSG_SAY_HELLO = 1;
 
     /**
-     * Handler of incoming messages from clients.
+     * Handler of incoming messages from clients.  normally it might do something here.  this just toasts.
      */
-    class IncomingHandler extends Handler {
+    Handler incomingHandler = new Handler(Looper.getMainLooper(), new Handler.Callback() {
         @Override
-        public void handleMessage(Message msg) {
+        public boolean handleMessage(@NonNull Message msg) {
             Log.v("MsgService", "Message received");
-            switch (msg.what) {
-                case MSG_SAY_HELLO:
+            if (msg.what == MSG_SAY_HELLO)
                     Toast.makeText(getApplicationContext(), "hello!", Toast.LENGTH_SHORT).show();
-                    break;
-                default:
-                    super.handleMessage(msg);
-            }
+            return true;
         }
-    }
+    });
 
     /**
      * Target we publish for clients to send messages to IncomingHandler.
      */
-    final Messenger mMessenger = new Messenger(new IncomingHandler());
+    final Messenger mMessenger = new Messenger(incomingHandler);
 
     /**
      * When binding to the service, we return an interface to our messenger
